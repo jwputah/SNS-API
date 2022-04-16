49aa3bea775bc411513127d092e6e75153cdb385
jwputah <jwputah7@gmail.com>
2022-04-15 19:10:32 -0600

    fixes in server.js and index.js added api routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
-const usersRoutes = require('./user-routes');
-const thoughtsRoutes = require('./thought-routes');
+const userRoutes = require('./user-routes');
+const thoughtRoutes = require('./thought-routes');
 
 // add prefix of `/users` to routes created in `user-routes.js`
-router.use('/users', usersRoutes);
+router.use('/users', userRoutes);
 // add prefix of `/thoughts` to routes created in `thought-routes.js`
-router.use('/thoughts', thoughtsRoutes);
+router.use('/thoughts', thoughtRoutes);
 
 module.exports = router;
---
+++ routes/api/thought-routes.js
@@ -0,0 +1,30 @@
+const router = require('express').Router();
+const {
+    getAllthoughts,
+    createThought,
+    getThoughtById,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction
+} = require('../../controllers/comment-controller');
+
+// /api/thoughts
+router
+    .route('/')
+    .get(getAllthoughts)
+    .post(createThought);
+
+// /api/thoughts/:id
+router
+    .route('/:id')
+    .get(getThoughtById)
+    .put(updateThought)
+    .delete(deleteThought);
+
+router
+    .route('/:thoughtId/reactions')
+    .post(addReaction)
+    .delete(deleteReaction);
+
+module.exports = router;
