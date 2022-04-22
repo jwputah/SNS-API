f1b91467af30ee833b39da1d7720f0693663efac
jwputah <jwputah7@gmail.com>
2022-04-21 20:32:19 -0600

    fixes for user and thought controllers

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -26,7 +26,6 @@ const thoughtController = {
     },
 
     createThought({ body }, res) {
-        console.log(body);
         Thought.create(body)
             .then((thoughtData) => {
                 return User.findOneAndUpdate(
@@ -35,13 +34,7 @@ const thoughtController = {
                     { new: true }
                 );
             })
-            .then((dbUserData) => {
-                if (!dbUserData) {
-                    res.status(404).json({ message: "No User with this ID" });
-                    return;
-                }
-                res.json(dbUserData);
-            })
+            .then(dbUserData => res.json(dbUserData))
             .catch(err => {
                 console.log(err);
                 res.status(400).json(err);
@@ -49,7 +42,7 @@ const thoughtController = {
     },
 
     updateThought({ params, body }, res) {
-        Thought.findOneAndUpdate({ _id: params.thoughtId }, body, { new: true, runValidators: true })
+        Thought.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
             .then(dbThoughtData => {
                 if (!dbThoughtData) {
                     res.status(404).json({ message: 'No thought found with this id.' });
@@ -67,7 +60,7 @@ const thoughtController = {
         Thought.findOneAndDelete({ _id: params.id })
             .then((dbThoughtData) => {
                 if (!dbThoughtData) {
-                    res.status(404).json({ message: "No thought with this ID" });
+                    res.status(404).json({ message: "No thought with this id." });
                     return;
                 }
                 res.json(dbThoughtData);
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -97,7 +97,7 @@ const userController = {
         )
             .then((dbUserData) => {
                 if (!dbUserData) {
-                    res.status(404).json({ message: "no user found with this ID" });
+                    res.status(404).json({ message: "no user found with this id." });
                     return;
                 }
                 res.json(dbUserData);
